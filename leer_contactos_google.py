68cdf5a0e881f6ca45c540114b70d304bb4e18b9
Javi <javicu25@gmail.com>
2025-04-22 14:30:05 +0000

    A medias

--- leer_contactos_google.py
+++ leer_contactos_google.py
@@ -10,5 +10,31 @@ print(df5.columns)
 # Iteración por filas del DataFrame:
 print("Iterando filas")
 for i in range(len(df5)):
-	col = df5.columns[0]
-	print(f"{col} -> {df5.iloc[i][col]}")
+	fila = df5.iloc[i]
+	# Reemplazar NaN con un valor temporal (por ejemplo, -999)
+	valor_temporal = -999
+	fila_con_relleno = fila.fillna(valor_temporal)
+	# Recorrer la fila sin leer NaN
+    valo6res = [valor for valor in fila_con_relleno if valor != valor_temporal]
+    
+    for col in df5.columns:
+		if df5.iloc[i][col].isna():
+			dato = df5.iloc[i][col]
+			fila += f"{col}: {dato} - " 
+	print(fila)
+	exit()
+	
+    # Obtener la fila deseada (por ejemplo, la primera fila)
+fila = df.iloc[0]
+
+# Reemplazar NaN con un valor temporal (por ejemplo, -999)
+valor_temporal = -999
+fila_con_relleno = fila.fillna(valor_temporal)
+
+# Recorrer la fila sin leer NaN
+valores = [valor for valor in fila_con_relleno if valor != valor_temporal]
+
+    # if dato == '':
+	# 	print("nulo")
+#	col = df5.columns[0]
+#	print(f"{col} -> {df5.iloc[i][col]}")
